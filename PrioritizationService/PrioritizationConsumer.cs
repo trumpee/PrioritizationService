e34fc3f5ba11a9b37acfddb0915385a795c35aef
Bardin08 <vbardin810@gmail.com>
2024-05-18 03:36:51 +0300

    Port PrioritizationService

---
+++ PrioritizationService/PrioritizationConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using Trumpee.MassTransit.Messages.Notifications;
+
+namespace Prioritize;
+
+public class PrioritizationConsumer : IConsumer<Notification>
+{
+    public async Task Consume(ConsumeContext<Notification> context)
+    {
+        var message = context.Message;
+        Console.WriteLine(
+            $"Received message: {message.Timestamp}-{message.Priority}");
+
+        // TODO: Add prioritization logic here...
+        var queueName = message.Priority switch
+        {
+            Priority.Low => "delivery-low",
+            Priority.Medium => "delivery-medium",
+            Priority.High => "delivery-high",
+            Priority.Critical => "delivery-critical",
+            _ => "no-queue" // TODO: write to DLQ
+        };
+
+        await context.Send(new Uri($"queue:{queueName}"), context.Message);
+    }
+}
---
+++ PrioritizationService/Program.cs
@@ -0,0 +1,52 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Prioritize;
+using Serilog;
+using Serilog.Events;
+using Trumpee.MassTransit;
+using Trumpee.MassTransit.Configuration;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("MassTransit", LogEventLevel.Debug)
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var host = CreateHostBuilder(args).Build();
+await host.RunAsync();
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureHostConfiguration(config =>
+        {
+            config.AddEnvironmentVariables();
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
+        })
+        .UseSerilog()
+        .ConfigureServices((host, services) =>
+        {
+            var rabbitTopologyBuilder = new RabbitMqTransportConfigurator();
+            rabbitTopologyBuilder.AddExternalConfigurations(x =>
+            {
+                x.AddConsumer<PrioritizationConsumer>();
+            });
+
+            rabbitTopologyBuilder.UseExternalConfigurations((ctx, cfg) =>
+            {
+                cfg.ReceiveEndpoint("prioritize", e =>
+                {
+                    e.BindQueue = true;
+                    e.PrefetchCount = 16;
+
+                    e.UseConcurrencyLimit(4);
+                    e.ConfigureConsumer<PrioritizationConsumer>(ctx);
+                });
+            }); 
+            
+            services.AddConfiguredMassTransit(host.Configuration, rabbitTopologyBuilder);
+        });
+}
